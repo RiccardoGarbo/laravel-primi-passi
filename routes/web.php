597a571060d5679bd53d0d034d05322475d3aca9
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-03-06 15:25:12 +0100

    print dinamic message on page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $hello = ['message' => 'Hello World!!!'];
+
+    return view('home', $hello);
 });
